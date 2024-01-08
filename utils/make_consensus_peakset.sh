56692d2ca961c4ad74f9e694ae95d67ceed0727f
Nuwah12 <ngb23@pitt.edu>
2024-01-08 15:44:35 -0500

    fixes

--- utils/make_consensus_peakset.sh
+++ utils/make_consensus_peakset.sh
@@ -1,6 +1,12 @@
 ###############
 # This script will take in an arbitrary amount of BED files and return the Union of all regions
 
+if [[ $# -lt 1 ]]; then
+	echo "Please use as such: make_consensus_peakset.sh -p <path to peak files> [-n <prefix name for output file>]"
+	exit 1
+fi
+name="unioned"
+
 while getopts ":p:n:" opt; do
   case $opt in
     p) peaks="$OPTARG";;
