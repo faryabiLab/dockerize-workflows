798da012dd756ac6c48a27a4149dd885524cd52a
Noah <Nuwah12>
2025-09-22 16:19:11 -0400

    cleanup

--- test_data/download_annotation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Download yeast (R64-1-1) full genome fasta
-
-URL="https://ftp.ensemblgenomes.ebi.ac.uk/pub/fungi/release-62/gff3/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.62.gff3.gz"
-
-wget "${URL}"
