1b554d3af0d1c8c896c3c0b8b58eb243d1d070cd
Noah <Nuwah12>
2025-09-22 16:18:44 -0400

    download scripts

---
+++ test_data/download_fasta.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Download yeast (R64-1-1) full genome fasta
+
+URL="http://ftp.ensemblgenomes.org/pub/fungi/release-62/fasta/saccharomyces_cerevisiae/dna/Saccharomyces_cerevisiae.R64-1-1.dna_rm.toplevel.fa.gz"
+
+wget "${URL}"
